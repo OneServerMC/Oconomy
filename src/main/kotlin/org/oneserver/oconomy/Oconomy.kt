8d604a3a676a45e09a1263773062ce5f58a26592
みにどん <48941476+M1n1don@users.noreply.github.com>
2022-03-16 20:13:19 +0900

    add: データベースとテーブルを追加

--- src/main/kotlin/org/oneserver/oconomy/Oconomy.kt
+++ src/main/kotlin/org/oneserver/oconomy/Oconomy.kt
@@ -2,6 +2,15 @@ package org.oneserver.oconomy
 
 import hazae41.minecraft.kutils.bukkit.PluginConfigFile
 import hazae41.minecraft.kutils.bukkit.init
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.addLogger
+import org.jetbrains.exposed.sql.transactions.TransactionManager
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.oneserver.oconomy.table.Account
+import java.io.File
+import java.sql.Connection
 
 class Oconomy : AbstractOconomy()
 {
@@ -16,6 +25,23 @@ class Oconomy : AbstractOconomy()
 
         init(MainConfig)
         MainConfig.autoSave = true
+
+        val dbFolder: File = File(dataFolder, "/database")
+        if (!dbFolder.exists()) dbFolder.mkdirs()
+
+        val dbFile: File = File(dataFolder, "/database/oconomy.db")
+        if (!dbFile.exists()) dbFile.createNewFile()
+
+        Database.connect("jdbc:sqlite:${dbFile.path}", "org.sqlite.JDBC")
+        TransactionManager.manager.defaultIsolationLevel = Connection.TRANSACTION_SERIALIZABLE
+
+        transaction {
+            addLogger(StdOutSqlLogger)
+
+            SchemaUtils.create(
+                Account
+            )
+        }
     }
 
     object MainConfig: PluginConfigFile("config")
