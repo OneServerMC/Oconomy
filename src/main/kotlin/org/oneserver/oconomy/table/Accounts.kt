626a5de7ed410437ce853a60fd768b600d30763c
みにどん <48941476+M1n1don@users.noreply.github.com>
2022-03-16 21:57:32 +0900

    change: 金額のデータ型

--- src/main/kotlin/org/oneserver/oconomy/table/Accounts.kt
+++ src/main/kotlin/org/oneserver/oconomy/table/Accounts.kt
@@ -2,15 +2,14 @@ package org.oneserver.oconomy.table
 
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
-import java.math.BigInteger
 import java.util.UUID
 
-object Account : Table()
+object Accounts : Table()
 {
     val id: Column<Int> = integer("id").autoIncrement()
     override val primaryKey = PrimaryKey(id)
 
     val uniqueId: Column<UUID> = uuid("uniqueId")
 
-    val balance: Column<Long> = long("balance")
+    val balance: Column<Double> = double("balance")
 }
